0ec7ed3e5529f8ea382879b23b4a4d26433c24c5
Amol Patil <amolnarayanpatil08@gmail.com>
2020-05-30 19:43:09 +0530

    mongoose demo

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,25 +5,28 @@ const dboper=require('./operation');
 const url='mongodb://localhost:27017/';
 const dbname='confusion';
 
-mongoclient.connect(url,(err,client)=>{
-    assert.equal(err,null);
+mongoclient.connect(url).then((client)=>{
     console.log('Connected to the server');
     const db=client.db(dbname);
    
-    dboper.insertDocument(db,{name:"vadonut",description:'test'},'dishes',(result)=>{
+    dboper.insertDocument(db,{name:"vadonut",description:'test'},'dishes').then((result)=>{
         console.log('Insert Document:\n',result.ops);
-        dboper.findDocument(db,'dishes',(docs)=>{
+       return  dboper.findDocument(db,'dishes');
+    })
+    .then((docs)=>{
             console.log('Found Documents:\n',docs);
-            dboper.updateDocument(db,{name:'vadonut'},{description:'updated test'},'dishes',(result)=>{
+            return dboper.updateDocument(db,{name:'vadonut'},{description:'updated test'},'dishes');
+    })
+            .then((result)=>{
                 console.log('Updated Document:\n',result.result);
-                dboper.findDocument(db,'dishes',(docs)=>{
+                return dboper.findDocument(db,'dishes');})
+                .then((docs)=>{
                     console.log('Found Documents:\n',docs);
-                db.dropCollection('dishes',(result)=>{
+                return db.dropCollection('dishes');})
+                .then((result)=>{
                     console.log('Dropped collection: ',result)
-                    client.close();
-                });
-            });
-        });
-    });
-});
-});
+                   return client.close();
+                })
+                .catch((err)=>console.log(err));
+})
+.catch((err)=>console.log(err));
