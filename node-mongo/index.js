682a359d534ba311a0f94839d189e58f09f62691
Amol Patil <amolnarayanpatil08@gmail.com>
2020-05-30 11:27:12 +0530

    nodemongo demo with separete file for db ops

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const mongoclient=require('mongodb').MongoClient;
 const assert=require('assert');
+const dboper=require('./operation');
 
 const url='mongodb://localhost:27017/';
 const dbname='confusion';
@@ -8,21 +9,21 @@ mongoclient.connect(url,(err,client)=>{
     assert.equal(err,null);
     console.log('Connected to the server');
     const db=client.db(dbname);
-    const collection=db.collection('dishes');
-    collection.insertOne({"name":"pizza","description":"test"},(err,result)=>{
-        assert.equal(err,null);
-        console.log('After Insert:\n');
-        console.log(result.ops);
-
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-            console.log('Found:\n');
-            console.log(docs);
-
-        db.dropCollection('dishes',(err,result)=>{
-            assert.equal(err,null);
-            client.close();
-        })
-        })
-    })
-})
+   
+    dboper.insertDocument(db,{name:"vadonut",description:'test'},'dishes',(result)=>{
+        console.log('Insert Document:\n',result.ops);
+        dboper.findDocument(db,'dishes',(docs)=>{
+            console.log('Found Documents:\n',docs);
+            dboper.updateDocument(db,{name:'vadonut'},{description:'updated test'},'dishes',(result)=>{
+                console.log('Updated Document:\n',result.result);
+                dboper.findDocument(db,'dishes',(docs)=>{
+                    console.log('Found Documents:\n',docs);
+                db.dropCollection('dishes',(result)=>{
+                    console.log('Dropped collection: ',result)
+                    client.close();
+                });
+            });
+        });
+    });
+});
+});
